c9ea6aa39d10397a1d921ed1db67506cb25b6ca2
erozonachi <erozonachi.james@gmail.com>
2019-07-19 09:50:09 +0100

    Add validate-project-id function to the validator

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,5 +1,18 @@
 const projectModel = require('../projects/project-model');
 
 module.exports = {
-  
+  validateProjectId: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const project = await projectModel.findById(id);
+      if(project && project.name) {
+        req.project = project;
+        return next();
+      }
+      res.status(400).json({ message: 'Invalid project id' });
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
 };
