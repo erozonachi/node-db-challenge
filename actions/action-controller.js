e2f01ad6213f391d737d947beeb0d82a2f55b188
erozonachi <erozonachi.james@gmail.com>
2019-07-19 11:06:35 +0100

    Add read function to the action controller

--- actions/action-controller.js
+++ actions/action-controller.js
@@ -1,5 +1,16 @@
 const actionModel = require('./action-model');
 
 module.exports = {
-  
+  read: async (req, res) => {
+    try {
+      if(req.action) {
+        return res.status(200).json(req.action);
+      }
+
+      const actions = await actionModel.find();
+      res.status(200).json(actions);
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
 };
