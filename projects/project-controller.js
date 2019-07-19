1925a53843bf9fed28d33997b54c789e23f7692d
erozonachi <erozonachi.james@gmail.com>
2019-07-19 10:13:04 +0100

    Add read function to the project controller

--- projects/project-controller.js
+++ projects/project-controller.js
@@ -9,4 +9,18 @@ module.exports = {
       res.status(500).json({ error: 'server error' });
     }
   },
+
+  read: async (req, res) => {
+    try {
+      if(req.project) {
+        const actions = await projectModel.findProjectActions(req.project.id);
+        return res.status(200).json({ ...req.project, actions });
+      }
+
+      const projects = await projectModel.find();
+      res.status(200).json(projects);
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
 };
