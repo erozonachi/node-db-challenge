f5d440764ec2d4c7d7ebc558d6d8746bf4f7d5e2
erozonachi <erozonachi.james@gmail.com>
2019-07-19 10:07:24 +0100

    Add create function to the project controller

--- projects/project-controller.js
+++ projects/project-controller.js
@@ -1,3 +1,12 @@
 const projectModel = require('./project-model');
 
-module.exports = {};
+module.exports = {
+  create: async (req, res) => {
+    try {
+      const result = await projectModel.insert(req.body);
+      res.status(201).json(result);
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
+};
