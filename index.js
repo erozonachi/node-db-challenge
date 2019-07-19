0ecc39096d7894a9f86260da26d0675ab3cf5c16
erozonachi <erozonachi.james@gmail.com>
2019-07-19 11:12:41 +0100

    Use action route in the app entry file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const projectRoute = require('./projects/project-route');
+const actionRoute = require('./actions/action-route');
 
 const PORT = process.env.PORT || 5000;
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', projectRoute);
+server.use('/api/actions', actionRoute);
 
 server.listen(PORT, () => {
   console.log(`Server running on port:- ${PORT}...`);
